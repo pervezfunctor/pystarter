a78fc603a229e1967e37790327d64ae4a9eb68f6
Pervez Iqbal <pervezfunctor@gmail.com>
2022-01-28 02:19:33 +0530

    create_task example

--- pystarter/asyncex/task.py
+++ pystarter/asyncex/task.py
@@ -4,15 +4,28 @@ import asyncio
 async def foo():
     await asyncio.sleep(10)
     print("Foo!")
+    return 100
+
+
+# async def hello_world():
+#     task = asyncio.create_task(foo())
+#     print(task)
+#     await asyncio.sleep(5)
+#     print("Hello World!")
+#     await asyncio.sleep(10)
+#     print(task)
+
+
+def do_some_computation():
+    pass
 
 
 async def hello_world():
-    task = asyncio.create_task(foo())
-    print(task)
-    await asyncio.sleep(5)
-    print("Hello World!")
-    await asyncio.sleep(10)
-    print(task)
+    x = asyncio.create_task(foo())  # Task <- Future
+    y = asyncio.create_task(foo())
+    do_some_computation()
+    z = foo()
+    print(await asyncio.gather(x, y, z))
 
 
 asyncio.run(hello_world())
